7963e75ff302cead4948471a261a335f1ccaf496
Alex Vergara <alex.vergara.dev@gmail.com>
2024-04-07 01:25:22 +0200

    feat: Better Zellij build scripts for Windows

--- build.sh
+++ build.sh
@@ -5,6 +5,15 @@ config() {
     git --git-dir=$HOME/.cfg/ --work-tree=$HOME "$@"
 }
 
+# Build Neovim
+build_neovim() {
+    config submodule update --init --remote code/tools/neovim
+    cd code/tools/neovim
+    mingw32-make deps
+    mingw32-make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX="$HOME"
+    mingw32-make install
+}
+
 # Install Neovim
 install_editor() {
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -14,14 +23,98 @@ install_editor() {
     fi
 }
 
+setup_zsh() {
+    echo "Setting up Zsh..."
+    pacman -S zsh
+    ## config submodule update --init --remote code/third_party/oh-my-zsh
+    git clone https://github.com/zsh-users/zsh-completions.git
+    # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    # git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    exec zsh
+}
+
+gh_cli_windows() {
+    echo "Setting up GitHub CLI for Windows..."
+    config clone https://github.com/cli/cli.git gh-cli
+    cd gh-cli || exit
+    go run script\build.go
+}
+
+gh_cli_linux() {
+    echo "Setting up GitHub CLI for Linux..."
+    config clone https://github.com/cli/cli.git gh-cli
+    cd gh-cli || exit
+    bin/gh version
+}
+
+gh_cli() {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	gh_cli_linux
+    else
+	gh_cli_windows
+    fi
+}
+
+update_llvm_suite() {
+    echo "Updating the LLVM project suite..."
+    config submodule update --init --remote code/third_party/llvm-project
+}
+
+build_llvm_suite() {
+    echo "Building LLVM suite..."
+    cd ~/code/third_party/llvm-project || exit
+    mkdir -p build
+    cmake -G Ninja -S runtimes -B build -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS='clang;lld;clang-tools-extra;lldb' -DLLVM_USE_LINKER=lld -DLLVM_PARALLEL_COMPILE_JOBS=8 -DLLVM_PARALLEL_LINK_JOBS=8 -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind"
+    ninja -C build
+}
+
+# Alacritty
+function install_alacritty() {
+    cargo install alacritty
+}
+
+function build_alacritty() {
+    config submodule update --init --remote --recursive code/tools/wezterm
+    cd code/tools/alacritty     
+    cargo build --release                                       
+    cd ~
+}
+
+# Zellij
+function install_zellij() {
+    cargo install zellij --locked
+}
+
+function build_zellij() {
+    config submodule update --init --remote --recursive code/tools/zellij
+    cd code/tools/zellij
+    cargo build --release
+    cd ~
+}
+
+# Currently only builds yet from the pr referenced below
+# Terminal multiplexer. It doens't work yet on Windows:
+# but looks promising: https://github.com/zellij-org/zellij/pull/2926
+function build_zellij_win() {
+    config submodule update --init --remote --recursive code/third-party/zellij
+    cd code/third-party/zellij
+    cargo build --release
+    cd ~
+}
+
+# Wezterm
+function build_wezterm() {
+    config submodule update --init --remote --recursive code/tools/wezterm
+    cd code/tools/wezterm
+    ./get-deps
+    cargo build --release
+    cd ~
+}
+
 # Install all the CMD utilities directly with Cargo
 terminal_tools() {
     # Here lives the ones available only on Linux
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        # Terminal multiplexer. It doens't work yet on Windows:
-        # but looks promising: https://github.com/zellij-org/zellij/pull/2926
-        echo "Installing Zellij..."
-        cargo install zellij --locked
         # Improved `rm` command
         echo "Installing a better rm command. rm-improved..."
         cargo install rm-improved
@@ -72,73 +165,28 @@ terminal_tools() {
     cargo install tokei
 }
 
-setup_zsh() {
-    echo "Setting up Zsh..."
-    pacman -S zsh
-    ## config submodule update --init --remote code/third_party/oh-my-zsh
-    git clone https://github.com/zsh-users/zsh-completions.git
-    # git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    # git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    exec zsh
-}
-
-gh_cli_windows() {
-    echo "Setting up GitHub CLI for Windows..."
-    config clone https://github.com/cli/cli.git gh-cli
-    cd gh-cli || exit
-    go run script\build.go
-}
-
-gh_cli_linux() {
-    echo "Setting up GitHub CLI for Linux..."
-    config clone https://github.com/cli/cli.git gh-cli
-    cd gh-cli || exit
-    bin/gh version
-}
-
-update_llvm_suite() {
-    echo "Updating the LLVM project suite..."
-    config submodule update --init --remote code/third_party/llvm-project
-}
-
-build_llvm_suite() {
-    echo "Building LLVM suite..."
-    cd ~/code/third_party/llvm-project || exit
-    mkdir -p build
-    cmake -G Ninja -S runtimes -B build -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_PROJECTS='clang;lld;clang-tools-extra;lldb' -DLLVM_USE_LINKER=lld -DLLVM_PARALLEL_COMPILE_JOBS=8 -DLLVM_PARALLEL_LINK_JOBS=8 -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind"
-    ninja -C build
-}
-
-
-function update_wezterm() {
-    config submodule update --init --remote --recursive code/third-party/wezterm  # Adjust 'code/third-party/wezterm' if needed
-    cd code/third_party/wezterm                                        # Navigate to the submodule directory
-    ./get-deps                                               # Fetch dependencies
-    cargo build --release                                    # Build wezterm in release mode
-    cd ~
-}
-
-
-function update_zellij() {
-    config submodule update --init --remote --recursive code/third-party/zellij
-    cd code/third_party/zellij
-    cargo build --release
-    cd ~
-}
-
 # Check the arguments to determine which component to build
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -z|--zsh) setup_zsh ;;
-        -nv|--neovim) install_editor ;;
-        -g|--gh-cli-windows) gh_cli_windows ;;
-        -l|--gh-cli-linux) gh_cli_linux ;;
-        -ullvm|--update-llvm-suite) update_llvm_suite ;;
+        -zsh|--setup_zsh) setup_zsh ;;
+	-bnv|--build-neovim) build_neovim ;;
+        -inv|--install-neovim) install_editor ;;
+        
+	-g|--gh-cli) gh_cli ;;
+        
+	-ullvm|--update-llvm-suite) update_llvm_suite ;;
         -bllvm|--build-llvm-suite) build_llvm_suite ;;
-        -uw|--update_wezterm) update_wezterm ;;
-        -uz|--update_zellij) update_zellij ;;
-        -tt|--terminal-tools) terminal_tools ;;
+	
+	-ia|--install_alacritty) install_alacritty ;;
+	-ba|--build_alacritty) build_alacritty ;;
+	
+	-iz|--iz_zellij) install_zellij ;;
+        -bz|--build_zellij) build_zellij ;;
+	-bzw|--build_zellij_win) build_zellij_win ;;
+	
+	-bw|--build_wezterm) build_wezterm ;;
+        
+	-tt|--terminal-tools) terminal_tools ;;
         *) echo "Unknown option: $1"; exit 1 ;;
     esac
     shift
